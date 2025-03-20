0b8790dca02742c27b8d54a5b9122276b727e3b4
slipperking <kurokhestwo@gmail.com>
2025-03-19 22:41:28 -0700

    Use rust iterators

--- src/devices/motor_group.rs
+++ src/devices/motor_group.rs
@@ -16,23 +16,32 @@ impl MotorGroup {
     pub fn new(motors: Vec<Motor>) -> Self {
         Self { motors }
     }
-    pub fn set_voltage_all(&mut self, voltage: f64) {
-        for motor in self.motors.iter_mut() {
-            let _ = motor.set_voltage(voltage);
-        }
+    pub fn set_voltage_all(&mut self, voltage: f64) -> Vec<Result<(), MotorError>> {
+        self.motors
+            .iter_mut()
+            .map(|motor| motor.set_voltage(voltage))
+            .collect()
     }
-    pub fn set_voltage_all_for_types(&mut self, voltage_v5: f64, voltage_exp: f64) {
-        for motor in self.motors.iter_mut() {
-            let _ = motor.set_voltage(match motor.motor_type() {
-                MotorType::V5 => voltage_v5,
-                MotorType::Exp => voltage_exp,
-            });
-        }
+    pub fn set_voltage_all_for_types(
+        &mut self,
+        voltage_v5: f64,
+        voltage_exp: f64,
+    ) -> Vec<Result<(), MotorError>> {
+        self.motors
+            .iter_mut()
+            .map(|motor| {
+                motor.set_voltage(match motor.motor_type() {
+                    MotorType::V5 => voltage_v5,
+                    MotorType::Exp => voltage_exp,
+                })
+            })
+            .collect()
     }
-    pub fn set_velocity_all(&mut self, rpm: i32) {
-        for motor in self.motors.iter_mut() {
-            let _ = motor.set_velocity(rpm);
-        }
+    pub fn set_velocity_all(&mut self, rpm: i32) -> Vec<Result<(), MotorError>> {
+        self.motors
+            .iter_mut()
+            .map(|motor| motor.set_velocity(rpm))
+            .collect()
     }
     // Use this for braking.
     pub fn set_target_all(&mut self, target: MotorControl) -> Vec<Result<(), MotorError>> {
